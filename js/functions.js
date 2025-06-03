1f22fa9493f91d76492e94bc461a5791ae77a90b
UrranQx <ucenikmarat@gmail.com>
2025-06-03 23:35:11 +0300

    module2 task1

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+function compareLength(string, maxLength){
+  return string.length() <= maxLength;
+}
+
+function isPalindrome(string){
+  string = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+  for (let i = string.length - 1; i >= 0; i--){
+    reverseString+=string.at(i);
+  }
+  return string === reverseString;
+}
